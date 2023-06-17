c74b50c81e77eeefd70b30ba78b81b54bc8bebab
samvrny <samvrny@gmail.com>
2023-06-17 12:32:48 -0500

    wrote first state store for opening and closing the modal. Got the modal to open.

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Auth Modal -->
-    <div class="fixed z-10 inset-0 overflow-y-auto hidden" id="modal">
+    <div class="fixed z-10 inset-0 overflow-y-auto" id="modal" :class="hiddenClass">
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div class="fixed inset-0 transition-opacity">
                 <div class="absolute inset-0 bg-gray-800 opacity-75"></div>
@@ -117,7 +117,13 @@
 </template>
 
 <script>
-export default {
-    name: 'AppAuth'
-}
+    import { mapState } from 'pinia';
+    import useModalStore from '@/stores/modal';
+
+    export default {
+        name: 'AppAuth',
+        computed: {
+            ...mapState(useModalStore, ['hiddenClass'])
+        }
+    }
 </script>
