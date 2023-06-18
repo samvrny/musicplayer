329cfc797014afd85c0236076b20576e7b1b6d9b
samvrny <samvrny@gmail.com>
2023-06-18 12:16:25 -0500

    finished setting up state management. Added functionality to the login and register forms, allowing a user to toggle between them

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -17,7 +17,7 @@
                     <div class="flex justify-between items-center pb-4">
                         <p class="text-2xl font-bold">Your Account</p>
                         <!-- Modal Close Button -->
-                        <div class="modal-close cursor-pointer z-50">
+                        <div class="modal-close cursor-pointer z-50" @click="modalVisibility = false">
                             <i class="fas fa-times"></i>
                         </div>
                     </div>
@@ -25,16 +25,35 @@
                     <!-- Tabs -->
                     <ul class="flex flex-wrap mb-4">
                         <li class="flex-auto text-center">
-                            <a class="block rounded py-3 px-4 transition hover:text-white text-white bg-blue-600"
-                                href="#">Login</a>
+                            <a  
+                                class="block rounded py-3 px-4 transition"
+                                href="#"
+                                @click.prevent="tab = 'login'"
+                                :class="{ 
+                                    'hover:text-white text-white bg-blue-600' : tab === 'login',
+                                    'hover:text-blue-600' : tab === 'register'
+                                }"
+                            >
+                            Log In
+                            </a>
                         </li>
                         <li class="flex-auto text-center">
-                            <a class="block rounded py-3 px-4 transition" href="#">Register</a>
+                            <a 
+                                class="block rounded py-3 px-4 transition" 
+                                href="#"
+                                @click.prevent="tab = 'register'"
+                                :class="{ 
+                                    'hover:text-white text-white bg-blue-600' : tab === 'register',
+                                    'hover:text-blue-600' : tab === 'login'
+                                }"
+                            >
+                            Register
+                            </a>
                         </li>
                     </ul>
 
                     <!-- Login Form -->
-                    <form>
+                    <form v-show="tab === 'login'">
                         <!-- Email -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Email</label>
@@ -55,7 +74,7 @@
                         </button>
                     </form>
                     <!-- Registration Form -->
-                    <form>
+                    <form v-show="tab === 'register'">
                         <!-- Name -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Name</label>
@@ -117,13 +136,21 @@
 </template>
 
 <script>
-    import { mapState } from 'pinia';
+    import { mapState, mapWritableState } from 'pinia';
     import useModalStore from '@/stores/modal';
 
     export default {
         name: 'AppAuth',
+        data() {
+            return {
+                tab: 'login',
+            }
+        },
         computed: {
-            ...mapState(useModalStore, ['hiddenClass'])
+            ...mapState(useModalStore, ['hiddenClass']),
+            ...mapWritableState(useModalStore, {
+                'modalVisibility': 'isOpen'
+            })
         }
     }
 </script>
