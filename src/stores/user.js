ed47843caeeace8d04b3814929f8cf8e6e4ad78b
samvrny <samvrny@gmail.com>
2023-06-20 15:44:20 -0500

    connected the user and their database together with the uid from firebase and using the doc() function frovided by Firebase.

--- src/stores/user.js
+++ src/stores/user.js
@@ -7,17 +7,21 @@ export default defineStore("user", {
     }),
     actions: {
         async register(values) {
-            await auth.createUserWithEmailAndPassword(
+            const userCredentials = await auth.createUserWithEmailAndPassword(
                 values.email, values.password
             );
 
-            await usersCollection.add({
+            await usersCollection.doc(userCredentials.user.uid).set({
                 name: values.name,
                 email: values.email,
                 age: values.age,
                 country: values.country,
                 tos: values.tos
             });
+
+            await userCredentials.user.updateProfile({
+                displayName: values.name,
+            })
 
             this.userLoggedIn = true;
         }
