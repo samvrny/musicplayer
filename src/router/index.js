6dadfb316947bf85ef55adafff35db9ea78908e0
samvrny <samvrny@gmail.com>
2023-06-22 16:01:03 -0500

    added style to active links. Got the manage page template imported. Got the router fully functional so one may switch between pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
-import About from '@/views/About.vue'
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
 
 const routes = [
   {
@@ -10,12 +11,17 @@ const routes = [
   {
     path: '/about',
     component: About
+  },
+  {
+    path: '/manage',
+    component: Manage
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 });
 
 export default router;
