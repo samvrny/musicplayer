f8aef61c1ec14eaec80fb65cde65563860402858
samvrny <samvrny@gmail.com>
2023-06-26 20:35:03 -0500

    finished for the night. Added validation to the edit song form using VeeValidate

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -13,33 +13,41 @@
             </button>
         </div>
         <div v-show="showForm">
-            <form>
+            <vee-form :validation-schema="schema" :initial-values="song" @submit="edit">
                 <div class="mb-3">
                     <label class="inline-block mb-2">Song Title</label>
-                    <input type="text"
+                    <vee-field type="text"
                         class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                        placeholder="Enter Song Title" />
+                        placeholder="Enter Song Title" 
+                        name="modifiedName"/>
+                    <ErrorMessage class="text-red-600" name="modifiedName" />
                 </div>
                 <div class="mb-3">
                     <label class="inline-block mb-2">Genre</label>
-                    <input type="text"
+                    <vee-field type="text"
                         class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                        placeholder="Enter Genre" />
+                        placeholder="Enter Genre" 
+                        name="genre"/>
+                    <ErrorMessage class="text-red-600" name="genre" />
                 </div>
                 <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">
                     Submit
                 </button>
-                <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">
+                <button 
+                    type="button" 
+                    class="py-1.5 px-3 rounded text-white bg-gray-600"
+                    @click.prevent="showForm = !showForm"
+                >
                     Go Back
                 </button>
-            </form>
+            </vee-form>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'CompositionItem',
+    name: "CompositionItem",
     props: {
         song: {
             type: Object,
@@ -48,7 +56,16 @@ export default {
     },
     data() {
         return {
-            showForm: false
+            showForm: false,
+            schema: {
+                modifiedName: "required|min:3|max:100",
+                genre: "min:3|max:100|alpha_spaces"
+            }
+        };
+    },
+    methods: {
+        edit(formData) {
+            console.log(formData)
         }
     }
 }
