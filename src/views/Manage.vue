7b18f6aef7188d498919c258b6824493b9fee17b
samvrny <samvrny@gmail.com>
2023-06-26 19:28:13 -0500

    added logic to query the database for songs. Added a new component for displaying the songs called CompositionItem. Preparing to print the songs to the page.

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -13,83 +13,7 @@
                     </div>
                     <div class="p-6">
                         <!-- Composition Items -->
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                            <div>
-                                <form>
-                                    <div class="mb-3">
-                                        <label class="inline-block mb-2">Song Title</label>
-                                        <input type="text"
-                                            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                            placeholder="Enter Song Title" />
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="inline-block mb-2">Genre</label>
-                                        <input type="text"
-                                            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                            placeholder="Enter Genre" />
-                                    </div>
-                                    <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">
-                                        Submit
-                                    </button>
-                                    <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">
-                                        Go Back
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                        </div>
+                        <composition-item v-for="song in songs" :key="song.documentId"></composition-item>
                     </div>
                 </div>
             </div>
@@ -99,17 +23,40 @@
 
 <script>
 import AppUpload from '@/components/Upload.vue'
+import CompositionItem from '@/components/CompositionItem.vue'
+import { songsCollection, auth } from '@/includes/firebase'
+
 export default {
     name: 'Manage',
+    data() {
+        return {
+            songs: []
+        }
+    },
     components: {
         AppUpload,
+        CompositionItem
     },
-    // beforeRouteLeave(to, from, next) {
-    //     this.$refs.upload.cancelUploads();
-    //     next();
-    // }
+    async created() {
+        const snapshot =await songsCollection.where('userId', '==', auth.currentUser.uid).get();
+        
+        snapshot.forEach((document) => {
+            const song = {
+                ...document.data(),
+                documentId: document.id,
+            }
+
+            this.songs.push(song);
+        })
+    }
+
 }
 </script>
+
+<!-- // beforeRouteLeave(to, from, next) { TODO: This is an example of using references. 
+    //     this.$refs.upload.cancelUploads();
+    //     next();
+    // } -->
 
 <!-- //TODO: remove the below code. For anyone looking at this: The below code is left in as an example to myself on guarding routes within a component. This code was in and would be put back into the script block if I wished to use it again. 
 // beforeRouteEnter(to, from, next) {
