792490d4f8ca45a41fb220a670614a1fda0729f9
samvrny <samvrny@gmail.com>
2023-06-28 20:31:49 -0500

    got the audio player working completely. Got the progress bar animating and the time for audio play formatted properly

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,13 +1,22 @@
 import { defineStore } from 'pinia';
 import { Howl } from 'howler';
+import helper from '@/includes/helper';
 
 export default defineStore('player', {
     state: () => ({
         currentSong: {},
-        sound: {}
+        sound: {},
+        seek: '00:00',
+        duration: '00:00',
+        playerProgress: '0%',
     }),
     actions: {
         async newSong(song) {
+
+            if(this.sound instanceof Howl) {
+                this.sound.unload();
+            }
+
             this.currentSong = song;
 
             this.sound = new Howl({
@@ -16,6 +25,40 @@ export default defineStore('player', {
             });
 
             this.sound.play();
+
+            this.sound.on('play', () => {
+                requestAnimationFrame(this.progress);
+            })
+        },
+        async toggleAudio() {
+            if(!this.sound.playing) {
+                return;
+            }
+
+            if(this.sound.playing()) {
+                this.sound.pause();
+            } else {
+                this.sound.play();
+            }
+        },
+        progress() {
+            this.seek = helper.formatTime(this.sound.seek());
+            this.duration = helper.formatTime(this.sound.duration());
+
+            this.playerProgress = `${(this.sound.seek() / this.sound.duration()) * 100}%`
+
+            if(this.sound.playing()) {
+                requestAnimationFrame(this.progress);
+            }
+        }
+    },
+    getters: {
+        playing: (state) => {
+            if(state.sound.playing) {
+                return state.sound.playing()
+            }
+
+            return false;
         }
     }
 });
