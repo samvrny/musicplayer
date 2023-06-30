af623565d046374575237a0f83316ee6ef0b5c30
samvrny <samvrny@gmail.com>
2023-06-29 21:31:33 -0500

    commented out the lorem ipsum on the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,11 @@
       <div class="container mx-auto">
         <div class="text-white main-header-content">
           <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
-          <p class="w-full md:w-8/12 mx-auto">
+          <!-- <p class="w-full md:w-8/12 mx-auto">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et dolor mollis, congue
             augue non, venenatis elit. Nunc justo eros, suscipit ac aliquet imperdiet, venenatis et
             sapien. Duis sed magna pulvinar, fringilla lorem eget, ullamcorper urna.
-          </p>
+          </p> -->
         </div>
       </div>
 
